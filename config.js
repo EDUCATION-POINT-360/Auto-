05f99f25e4a6bdd15fa28b95fcf1e2a7114d025b
EDUCATION-POINT-360 <mrizwank585@gmail.com>
2026-03-04 08:59:32 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  matricLink: "https://chat.whatsapp.com/Lefqjm3CwGOFw07fzvjAUZ",
+  intermediateLink: "https://chat.whatsapp.com/JVWUSeZBVrb3iZRAdkp7e8",
+  officialLinks: {
+    instagram: "https://www.instagram.com/educ.ationpointofficial?igsh=MWtkOWEyM2o2cjltbg==",
+    facebook: "https://www.facebook.com/share/1C3r1Jy8QH/",
+    whatsappNumber: "0331-4220506",
+    whatsappChannel: "https://whatsapp.com/channel/0029VayfTrh8PgsHHebiwc2k",
+    youtube: "https://youtube.com/@educationpoint998?si=Vnua9sOqbo4Dw_zj",
+    website: "https://educationpoint360.netlify.app/",
+    app: "https://apkpure.com/education-point/co.median.android.djoypo",
+    email: "educationpoint0360@gmail.com",
+    nurWebsite: "https://nurislamic.netlify.app/"
+  },
+  greeting: "Assalamu Alaikum 😊\nMain Muhammad Rizwan ka assistant bol raha hoon – Education Point ✨ aur NUR نور ki taraf se.\nAap kaise ho? Main aap ki kya madad kar sakta hoon?"
+};
